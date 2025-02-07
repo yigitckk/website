1f9c90e41f92e57e05710185c6e3940aa86940fc
Yigit Celik <yigitcelik798@gmail.com>
2025-02-07 22:26:11 +0300

    deployment

--- src/pages/CertificationsPage.tsx
+++ src/pages/CertificationsPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Certifications from '/home/yigit/portfolio-website-1/src/components/Certifications';
+import Certifications from '../components/Certifications';
+
 
 const CertificationsPage: React.FC = () => {
   return (
