4d8df4c451ba2b217d5e30a1147ead81b629dec4
Yigit Celik <yigitcelik798@gmail.com>
2025-07-19 16:16:59 +0300

    Update of July

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,7 +23,7 @@ const About: React.FC = () => {
         <Button
           variant="contained"
           color="primary"
-          href="https://github.com/yigitckk/aboutMe/blob/main/Yiğit%20çelik.pdf"
+          href="https://github.com/yigitckk/aboutMe/blob/main/YiğitÇelikcv.pdf"
           target="_blank"
           rel="noopener"
         >
@@ -31,7 +31,7 @@ const About: React.FC = () => {
         </Button>
       </Box>
       <Typography variant="body1" paragraph>
-        Yiğit Çelik is a 20-year-old university student based in Türkiye, known for his passion for innovation and problem-solving. With a strong academic foundation and an insatiable curiosity, Yiğit has consistently demonstrated a commitment to personal and professional growth through active participation in both national and international projects. His areas of expertise include mathematics, programming, languages, and philosophy, which he leverages to approach challenges with a unique blend of analytical thinking and creative problem-solving.
+        Yiğit Çelik is a 21-year-old university student based in Türkiye, known for his passion for innovation and problem-solving. With a strong academic foundation and an insatiable curiosity, Yiğit has consistently demonstrated a commitment to personal and professional growth through active participation in both national and international projects. His areas of expertise include mathematics, programming, languages, and philosophy, which he leverages to approach challenges with a unique blend of analytical thinking and creative problem-solving.
       </Typography>
       <Typography variant="body1" paragraph>
         Professionally, Yiğit is skilled in multiple programming languages, including HTML, JavaScript, CSS, C, C#, and is currently expanding his proficiency in Java. His technical acumen, combined with his ability to quickly adapt and resolve complex issues, positions him as a dynamic contributor in any team environment. Beyond his technical skills, Yiğit brings a wealth of cross-cultural experience, having participated in over seven cultural, youth, and student exchange programs under the Erasmus+ initiative. These experiences have honed his interpersonal skills, enabling him to thrive in collaborative settings and engage in meaningful, interactive discussions with individuals from diverse backgrounds.
